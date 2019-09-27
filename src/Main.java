849bf7da29439ba702bdbf73d204005905d65d76
mohammed el-orfali <Moel18ad@student.cbs.dk>
2019-09-27 11:14:14 +0200

    Mohammed har lavet aendringer

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ public class Main {
         String myUsername = "this is my username";
         String myPassword = "This is my password";
         System.out.println("Mo - han er benhård!");
+        System.out.println("Y han er beeeeeeen!!!!");
+        System.out.println("Omar han er arm");
+        System.out.println("Jeff han er lår");
+
 
         if(!User.validateUsername(myUsername)) {
             System.out.println("Username doesn't fit the rules. Please check the User class.");
